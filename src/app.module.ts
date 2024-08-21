dc38a8baeecacaaff687f57fa6bd24ecdc38c86f
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-08-21 22:03:26 +0600

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,12 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth/auth.guard';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017'),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.DB_URL),
     FeedbackModule,
     UsersModule,
     AuthModule,
