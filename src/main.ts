fed4caad9921ebca73f25dbe9f3984fced34710f
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-09-01 17:11:47 +0600

    swagger integrated

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { version } from '../package.json';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Customer Feedback Sentiment Analysis RestAPI')
+    .setDescription('API description')
+    .setVersion(version)
+    .addTag('FeedBack API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
